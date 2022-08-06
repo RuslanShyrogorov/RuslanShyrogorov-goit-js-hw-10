a59300fa068d5904457e638dcb1618652bede894
RuslanShyrogorov <ruslanshirogorov@gmail.com>
2022-08-06 21:53:04 +0300

    start task 10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-var debounce = require('lodash.debounce');
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+
+function handlerInput(e) {
+  const searchText = e.target.value.trim()
+  if (searchText === '') {
+    Notify.failure('Please, enter country name!');
+    return
+  }
+  console.log('searchText: ', searchText);
+}
+
+inputEl.addEventListener('input', debounce(handlerInput, DEBOUNCE_DELAY))
+
